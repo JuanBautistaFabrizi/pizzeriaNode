98d61cf20a63a8f3ba4232f37be30f41d368a384
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-01-28 18:03:34 -0300

    UserRegister added

--- Models/User.js
+++ Models/User.js
@@ -3,6 +3,7 @@ const {Schema,model} = require('mongoose');
 const UserSchema = new Schema({
     name: {type: String,required:true},
     status: {type:Boolean,required:true},
+    role: { type: String, required: true, enum: ['ADMIN_ROLE', 'CLIENT_ROLE'] },
     email: {type:String,required:true}
 })
 
