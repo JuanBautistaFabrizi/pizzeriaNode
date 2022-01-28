62f4aa8a826685889dbfff59c2e726d0eb4bf3e5
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-01-28 17:47:38 -0300

    role model added

---
+++ Controllers/auth.js
@@ -0,0 +1,17 @@
+const { genSaltSync, hashSync, compareSync } = require("bcryptjs");
+//const {generateJWT} =
+const User = require("../Models/User");
+
+const UserRegister = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const newUser = new User({ name, email, password, role });
+
+  const salt = genSaltSync(8);
+  newUser.password = hashSync(password, salt);
+
+  await newUser.save();
+
+  res.json({
+    newUser,
+  });
+};
