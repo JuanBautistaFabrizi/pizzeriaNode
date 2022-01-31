e56b47d260dc95583588615e7ec28d68a5c85ec7
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-01-31 14:28:54 -0300

    categories controllers added

--- Controllers/categories.js
+++ Controllers/categories.js
@@ -43,3 +43,32 @@ const createCategory = async (req,res) => {
     res.status(500).json(error);
   }
 }
+
+const updateCategory = async (req,res) => {
+  const {id} = req.params;
+  const {status,user, ...data} = req.body;
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(id,data);
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteCategory = async (req,res) => {
+  const {id} = req.params;
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(id,{status: false});
+    res.status(200).json(deletedCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+module.exports = {
+  allCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory
+}
